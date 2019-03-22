d5859ef36bfca571be3987acc6e91903dbecb347
Levi Lesches <levilesches@gmail.com>
2019-03-21 22:25:10 -0400

    Implemented Equation.getMatrix

--- lib/bases/equation.dart
+++ lib/bases/equation.dart
@@ -2,12 +2,14 @@ import "side.dart";
 import "element.dart";
 import "molecule.dart";
 import "../helpers/counter.dart";
+import "../helpers/matrix.dart";
 
 class Equation {
 	final Side left, right;
 	Counter<Element> elements;
+	final Matrix matrix;
 
-	Equation._ (this.left, this.right) {
+	Equation._ (this.left, this.right, this.matrix) {
 		verify();
 		this.elements = left.elements;  // just cuz
 	}
@@ -15,9 +17,9 @@ class Equation {
 	factory Equation (String equation) {
 		final List<String> sides = equation.split(" --> ");
 		assert (sides.length == 2, "Equation recieved too many parts: $equation");
-		final Side _left = Side (sides [0]);
-		final Side _right = Side (sides [1]); 
-		return Equation._(_left, _right);
+		final Side left = Side (sides [0]);
+		final Side right = Side (sides [1]); 
+		return Equation._(left, right, getMatrix(left, right));
 	}
 
 	@override String toString() => "$left --> $right";
@@ -34,7 +36,31 @@ class Equation {
 		) throw "There is an inconsistency in $this";
 	}
 
+	static Matrix getMatrix(Side left, Side right) {
+		final List<List<int>> matrix = [];
+		for (final CounterEntry<Element> element in left.elements) {
+			final List<int> row = [];
+			for (final CounterEntry<Molecule> molecule in left.molecules)
+				row.add (
+					(molecule.value.elementsList.contains(element.value))
+						? molecule.value.elements [element.value] * -1
+						: 0
+				);
+
+			for (final CounterEntry<Molecule> molecule in right.molecules) 
+				row.add (
+					(molecule.value.elementsList.contains (element.value))
+						? molecule.value.elements [element.value]
+						: 0
+				);
+
+			matrix.add (row);
+		}
+		return Matrix (matrix);
+	}
+	
 	bool get balanced => left.elements == right.elements;
+
 
 	void balance() {
 		final List<List<int>> matrix = [];
@@ -52,6 +78,7 @@ class Equation {
 			}
 			matrix.add (row);
 		}
+
 	}
 }
 
