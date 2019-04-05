4ff0e0ce7b5b4ed6768e54e55493aed8abee61f1
Levi Lesches <levilesches@gmail.com>
2019-04-04 23:03:05 -0400

    Fixed algorithm + cleanup

--- lib/bases/model.dart
+++ lib/bases/model.dart
@@ -1,35 +1,84 @@
 // import "element.dart";
 
 import "../helpers/range.dart";
+// import "dart:collection";
 // import "periodic_table.dart";
 
 class Node {
 	final String element;
 	final List<Node> bonds = [];
 	Node (this.element);
-	void addBond (List<Node> nodes) => bonds.addAll(nodes);
+	void addBonds (List<Node> nodes) => bonds.addAll(nodes);
+	static void bond (Node a, Object b) {
+		if (b is List<Node>) {
+			a.bonds.addAll (b);
+			for (final Node node in b) node.bonds.add (a);
+		}
+		else {
+			a.bonds.add (b);
+			(b as Node).bonds.add (a);
+		}
+	}
 }
 
 Iterable<MapEntry<int, V>> sortMap<V>(Map<int, V> map) sync* {
 	final int length = (map.length - 1) ~/ 2;
-	for (final int index in range (length + 1, start: -length)) yield MapEntry<int, V>(index, map [index]);
+	for (final int index in range (length + 1, start: -length)) 
+		yield MapEntry<int, V>(index, map [index]);
 }
 
-Map<int, List<Node>> paint(List<Node> nodes) {
+Iterable<MapEntry<int, List<Node>>> paint(List<Node> nodes) {
 	final Map<int, List<Node>> levels = {};
-	void recurse (Node pivot, {Node previous, int level = 0}) {
-		if (!levels.containsKey(level)) levels [level] = [];
-		levels [level].add (pivot);
-		if (pivot == null) return;
-		Map<int, Node> agenda = pivot.bonds.where(
-			(Node node) => node != previous
-		).toList().asMap();
-		for (final int index in range (3))
-			recurse (agenda [index], previous: previous, level: index - 1);
+	void expand(Node pivot, {int previous, int lastLevel, int level = 0}) {
+		if (!levels.containsKey (level)) {
+			if (previous != null) levels [level] = List.filled(previous, null);
+			else levels [level] = [];
+		}
+		levels [level].add(pivot);
+
+		final List<int> directions = [];
+		if (
+			levels.containsKey(level - 1) && 
+			levels [level - 1].length == levels [level].length
+		) directions.addAll ([level + 1, level, level]);
+		else if (
+			levels.containsKey(level + 1) &&
+			levels [level + 1].length == levels [level].length
+		) directions.addAll([level - 1, level, level]);
+		else directions.addAll ([level, level + 1, level - 1]);
+
+		final List<Node> agenda = pivot.bonds.where (
+			(Node node) => 
+				previous == null || node != levels [lastLevel] [previous]
+		);
+
+		final int index = levels [level].length - 1;
+		for (final int length in const [3, 2, 1]) {
+			if (agenda.length >= length) expand (
+				agenda [length - 1],
+				previous: index,
+				lastLevel: level, 
+				level: directions [length - 1]
+			);
+		}
 	}
 
-	recurse (nodes.firstWhere((Node node) => node.bonds.length == 1));
-	return levels;
+	expand (
+		nodes.reduce (
+			(Node a, Node b) => a.bonds.length < b.bonds.length ? a : b
+		)
+	);
+
+	final int maxLength = levels.values.map(
+		(List<Node> row) => row.length
+	).reduce (
+		(int a, int b) => a > b ? a : b
+	);
+
+	for (final int level in levels.keys) {
+		if (levels [level].length != maxLength) levels [level].add (null);
+	}
+	return sortMap(levels);
 }
 
 void main() {
@@ -41,13 +90,8 @@ void main() {
 	final Node h4 = Node("H");
 	final Node h5 = Node("H");
 	final Node h6 = Node("H");
-	c1.addBond([h1, h2, h4, c2]);
-	c2.addBond([c1, h3, h5, h6]);
-	h1.addBond([c1]);
-	h2.addBond([c1]);
-	h3.addBond([c2]);
-	h4.addBond([c1]);
-	h5.addBond([c2]);
-	h6.addBond([c2]);
-	print (paint ([c1, c2, h1, h2, h3, h4, h5, h6]));
+
+	Node.bond (c2, <Node>[h3, h5, h6]);
+	Node.bond (c1, <Node>[c2, h1, h2, h4]);
+	paint ([c1, c2, h1, h2, h3, h4, h5, h6]);
 }
